db9681b7f566c71c21c951912baebb91cb34f27d
Bryan <bryantb479@gmail.com>
2024-01-27 19:32:30 -0500

    listo splash screen

--- app/src/main/java/com/utc/cuentaregresiva/MainActivity.java
+++ app/src/main/java/com/utc/cuentaregresiva/MainActivity.java
@@ -46,7 +46,16 @@ public class MainActivity extends AppCompatActivity {
 
         img.setAnimation(top_anim);
         cronometro.setAnimation(bottom_anim);
-        System.out.println("Esto es un cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
+        System.out.println("Esto es dfdfdun cambio");
         // Crear la animacion
         new Handler().postDelayed(new Runnable() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)

20f270dcec270fab7e22777be9afbaa98aaf0b14
Bryan <bryantb479@gmail.com>
2024-01-27 22:06:31 -0500

    Menu Moderno con Fragmets Completado

--- app/src/main/java/com/utc/cuentaregresiva/MenuPrincipal.java
+++ app/src/main/java/com/utc/cuentaregresiva/MenuPrincipal.java
@@ -1,6 +1,7 @@
 package com.utc.cuentaregresiva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,16 +11,32 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.utc.cuentaregresiva.fragmentos.CrearEvento;
+import com.utc.cuentaregresiva.fragmentos.ListaEventos;
+import com.utc.cuentaregresiva.fragmentos.PerfilUsuario;
+
 public class MenuPrincipal extends AppCompatActivity {
     TextView txt_usuario;
 
     SharedPreferences preferencias;
     SharedPreferences.Editor editor;
 
+    ChipNavigationBar chipNavigationBar; // Barra de navegacion de abajo
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        // Relacionar los elementos logicos con los elementos graficos
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+        // Marcar por defecto la opcion ListaEventos en el Menu
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_dashboard, true);
+        // Se coloca como valor por defecto el Fragmento ListaEventos()
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ListaEventos()).commit();
+        bottomMenu();
+
 
         txt_usuario = (TextView) findViewById(R.id.txt_usuario);
 
@@ -27,6 +44,30 @@ public class MenuPrincipal extends AppCompatActivity {
         txt_usuario.setText("Bienvenido < "+ preferencias.getString("nombre_usuario", "") + " >");
         editor = preferencias.edit();
 
+    }
+
+    private void bottomMenu() {
+        // Este metodo se puede utilizar con la dependencia de :
+        // implementation 'org.jetbrains.kotlin:kotlin-stdlib:1.3.72'
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int pos) {
+                Fragment fragment = null;
+                switch (pos) {
+                    case R.id.bottom_nav_dashboard:
+                        fragment = new ListaEventos();
+                        break;
+                    case R.id.bottom_nav_manage:
+                        fragment= new CrearEvento();
+                        break;
+                    case R.id.bottom_nav_profile:
+                        fragment = new PerfilUsuario();
+                        break;
+                }
+                // Colocar el fragmento seleccionado del Menu
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
     }
 
     public void cerrarSesion(View view) {

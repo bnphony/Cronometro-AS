db0da67d8368adb74ffacd9b542d0f6fc97ae2e9
Bryan <bryantb479@gmail.com>
2024-11-16 21:51:30 -0500

    completado para github readme

--- app/src/main/java/com/utc/cuentaregresiva/fragmentos/NewPassword.java
+++ app/src/main/java/com/utc/cuentaregresiva/fragmentos/NewPassword.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -79,6 +81,7 @@ public class NewPassword extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_new_password, container, false);
 
